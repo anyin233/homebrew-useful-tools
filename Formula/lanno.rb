71ecd18aedde348b99a9fd2069659b87204a204f
anyin233 <shizukuprprpr@gmail.com>
2025-04-15 21:26:45 +0800

    chore: update description of lanno

--- Formula/lanno.rb
+++ Formula/lanno.rb
@@ -1,9 +1,9 @@
 class Lanno < Formula
-  desc "Lanno is a command-line tool designed to enhance code organization and documentation by providing annotations for files, akin to using the ls command but with additional metadata."
+  desc "A cli tool can add metadata to your directory."
   homepage "https://github.com/chivier/lanno"
   url "https://github.com/Chivier/lanno/archive/refs/tags/v1.1.0.tar.gz"
   sha256 "b7c765837e30e64b54292da701a117e544e0f12e70bae03bcf835bd310bc6878"
-  license "AGPL-3.0" # 或其他适用的许可证
+  license "AGPL-3.0" 
 
   depends_on "go" => :build
 
